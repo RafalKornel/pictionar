3c465011deeba5f4ca0fc5f5efa10d74c4dd1c87
RafalKornel <rafal.kornel123@gmail.com>
2020-10-19 15:08:05 +0200

    Full concept

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,17 @@
 const button = document.querySelector("#switch");
+const login  = document.querySelector("#login");
+const words  = document.querySelector("#words")
 const smallElement = document.querySelector("#smallElement");
 
 button.addEventListener("click", () => {
     if (smallElement.classList.contains("moved")){
         smallElement.classList.remove("moved");
+        login.style.display = "inherit";
+        words.style.display = "none";
     }
     else {
         smallElement.classList.add("moved");
+        login.style.display = "none";
+        words.style.display = "inherit";
     }
 })
