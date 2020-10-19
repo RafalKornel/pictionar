e57b4d7a7abfefac398edca5338dc1ecdcb34504
RafalKornel <rafal.kornel123@gmail.com>
2020-10-20 01:55:16 +0200

    Implemented very shitty version of messages in back

--- client/js/main.js
+++ client/js/main.js
@@ -2,6 +2,7 @@ const button = document.querySelector("#switch");
 const login  = document.querySelector("#login");
 const words  = document.querySelector("#wordsForm")
 const info   = document.querySelector("#info");
+const messages = document.querySelector("#messages");
 const smallElement = document.querySelector("#smallElement");
 
 button.addEventListener("click", () => {
@@ -10,12 +11,14 @@ button.addEventListener("click", () => {
         login.classList.add("hidden");
         words.classList.remove("hidden");
         info.classList.add("hidden");
+        messages.classList.remove("hidden");
     }
     else{
         smallElement.classList.remove("moved");
         login.classList.remove("hidden");
         words.classList.add("hidden"); 
         info.classList.remove("hidden");
+        messages.classList.add("hidden");
 
     }
 
