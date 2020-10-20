34f36e68e1ef7cb4e17b72fcdae8e61421d0dde0
RafalKornel <rafal.kornel123@gmail.com>
2020-10-20 19:31:20 +0200

    Finished second page style, and first page mobile style partially

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,6 @@
 const button = document.querySelector("#switch");
-const login  = document.querySelector("#login");
-const words  = document.querySelector("#wordsForm")
-const info   = document.querySelector("#info");
+const login  = document.querySelector("#loginContent");
+const words  = document.querySelector("#wordsContent");
 const messages = document.querySelector("#messages");
 const smallElement = document.querySelector("#smallElement");
 
@@ -11,14 +10,12 @@ button.addEventListener("click", () => {
         login.classList.add("hidden");
         words.classList.remove("hidden");
         info.classList.add("hidden");
-        messages.classList.remove("hidden");
     }
     else{
         smallElement.classList.remove("moved");
         login.classList.remove("hidden");
         words.classList.add("hidden"); 
         info.classList.remove("hidden");
-        messages.classList.add("hidden");
 
     }
 
