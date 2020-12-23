0f89edb960585a1e47dd89295950ebff4ed6b0ca
RafalKornel <rafal.kornel123@gmail.com>
2020-12-24 00:41:24 +0100

    [NEW] Implemented words retrieving functionality.

--- client/src/components/wordsInfo.jsx
+++ client/src/components/wordsInfo.jsx
@@ -5,10 +5,30 @@ class WordsInfo extends React.Component {
     constructor(props) {
         super(props);
 
+        this.setSuccessMessage = this.setSuccessMessage.bind(this);
+        this.unsetSuccessMessage = this.unsetSuccessMessage.bind(this);
+
         this.state = {
-            retrievedWords: [],
+            copySuccess: "",
             count: 0,
         }
+    }
+
+    componentDidMount() {
+        fetch("/api/count")
+        .then(res => {
+            if (res.ok) return res.text()} )
+        .then(text => this.setState({ count: text }))
+        .catch(err => console.error(err));
+    }
+
+    setSuccessMessage() {
+        this.setState({ copySuccess: "Database words have been copied to clipboard."})
+        setTimeout( () => this.unsetSuccessMessage(), 5000 );
+    }
+
+    unsetSuccessMessage() {
+        this.setState({ copySuccess: ""})
     }
 
     render() {
@@ -17,11 +37,35 @@ class WordsInfo extends React.Component {
                 <article>
                     <h2>There are <span className="wordsInfo__count">{this.state.count}</span> words in database.</h2>
                     <h2>Hit the button below to get words from database</h2>
+                    <h2 style={{ color: "var(--form-color)"}}>{this.state.copySuccess}</h2>
                 </article>
 
-                <button type="button" className="getWordsButton">Get words!</button>
+                <GetWordsButton showSuccessMessage={this.setSuccessMessage} />
             </div>
         );
+    }
+}
+
+class GetWordsButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        fetch("/api/bank")
+        .then( res => res.json() )
+        .then( data => {
+            navigator.clipboard.writeText(data);
+            this.props.showSuccessMessage();
+        })
+        .catch( err => console.error(err) )
+    }
+
+    render() {
+        return (<button type="button" className="getWordsButton" onClick={this.handleClick}>Get words!</button>);
+        
     }
 }
 
