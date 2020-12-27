c78df22bdb69606c8326c2704f4f62c4f21cc66f
RafalKornel <rafal.kornel123@gmail.com>
2020-12-27 02:51:52 +0100

    [FIX] missing import

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from ..config import config
+import os
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
